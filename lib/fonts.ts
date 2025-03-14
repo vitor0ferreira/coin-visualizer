e549b10584983245ba8f2e664fc8fff46a5334eb
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2025-03-14 11:04:44 -0300

    fix: add subsets on font Jersey10

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -6,12 +6,14 @@ export const Jersey10Font = Jersey_10({
     variable:"--font-Jersey-10",
     style:["normal"],
     weight:["400"],
-    preload:true
+    subsets:["latin"],
+    preload:true,
 })
 
 export const robotoFont = Roboto({
     variable: "--font-Roboto",
     style: ["italic", "normal"],
+    weight: ["100", "300", "400", "500", "700", "900"],
     subsets: ["latin"],
-    weight: ["100", "300", "400", "500", "700", "900"]
+    preload:true,
   })
